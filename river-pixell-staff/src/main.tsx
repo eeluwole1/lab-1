572aa744d4684659e1b80858a7ae3d9f0acfe513
eeluwole1 <engrwolesax@gmail.com>
2025-09-05 13:56:14 -0500

    Updated App.tsx

--- river-pixell-staff/src/main.tsx
+++ river-pixell-staff/src/main.tsx
@@ -1,11 +1,9 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App'
 
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>
 )
